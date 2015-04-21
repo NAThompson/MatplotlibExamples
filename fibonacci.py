0507209e93dbc03f88d41bc3724c96475cd6f0f9
Nicholas <nthompson@slb.com>
2015-04-21 16:04:45 -0500

    Cleanup

--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
 import math
+
 length = 25
-fib = numpy.ones(length)
-x = numpy.zeros(length)
+fib = np.ones(length)
+x = np.zeros(length)
 x[1] = 1
 for ii in range(2, len(fib)):
     fib[ii] = fib[ii-1] + fib[ii-2]
@@ -13,9 +14,11 @@ for ii in range(2, len(fib)):
 
 golden_ratio  = 1.61803398875
 
+# Default it to a log plot:
 plt.semilogy(x, fib, 'go', label='Fibonacci numbers')
 plt.semilogy(x, (golden_ratio**x)/math.sqrt(5), 'bd', label=r"$\phi^{n}/\sqrt{5}$")
 
+# Add a legend:
 legend = plt.legend(loc='upper left', shadow=True, fontsize='x-large')
 
 plt.title(r"Fibonnacci numbers are asymptotic to $\frac{\phi^{n}}{\sqrt{5}}$; an example using $\tt{plt.semilogy}$")
