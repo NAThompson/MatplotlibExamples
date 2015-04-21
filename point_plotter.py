5fec7af36f4d6968d01cbdb1414622e433ff643c
Nicholas <nthompson@slb.com>
2015-04-21 16:56:12 -0500

    Cleanup

--- point_plotter.py
+++ point_plotter.py
@@ -1,9 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import matplotlib.pyplot as plt
 
 def plot_point(event):
     plt.scatter(event.xdata, event.ydata)
+    print("Adding point at ({}, {})".format(event.xdata, event.ydata))
     plt.draw()
 
 fig, ax = plt.subplots(1,1)
