6f40d31c13621f4db55cb3d1d764c92656759e78
Nicholas <nthompson@slb.com>
2015-04-21 16:54:57 -0500

    Cleaning up torus.

--- torus.py
+++ torus.py
@@ -14,7 +14,7 @@ angle = np.linspace(0, 2*np.pi, 32)
 theta, phi = np.meshgrid(angle, angle)
 
 R = 4
-r = 1
+r = 1.2
 
 
 x = (R + r*np.cos(theta))*np.cos(phi)
@@ -24,9 +24,14 @@ z = r*np.sin(theta)
 ax.set_xlim3d(-R, R)
 ax.set_ylim3d(-R, R)
 ax.set_zlim3d(-R, R)
+ax.set_zlabel(r"$z = {}\sin(\theta)$".format(r))
 ax.plot_surface(x, y, z,  rstride=1, cstride=1, color='b')
 
 
+plt.title(r"Example use of mpl_toolkits.mplot3d: $x = ({} + {}\cos(\theta))\cos(\phi)$".format(R,r))
+plt.xlabel(r"$x = ({} + {}\cos(\theta))\cos(\phi)$".format(R,r))
+plt.ylabel(r"$y = ({} + {}\cos(\theta))\sin(\phi)$".format(R,r))
+#plt.zlabel(r"$z = {}\sin(\theta)".format(r))
 
 plt.show()
 
