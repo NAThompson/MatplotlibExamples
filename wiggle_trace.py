af2a1fecf4913023ec932ec238468e41f664d6b2
Nicholas <nthompson@slb.com>
2015-04-21 07:08:33 -0500

    Comments.

--- wiggle_trace.py
+++ wiggle_trace.py
@@ -1,22 +1,5 @@
 #!/usr/bin/env python3
 
-"""
-This script provides objects which allow the display of wiggly traces.
-
-The vision for this script, though not its current reality is:
-
-    --numpy arrays with associated receiver locations, with timestep and metadata, as input.
-    --wiggle traces displayed at the receiver location, in 3D.
-    --amplitudes dynamically scaled by user.
-    --metadata coloring of wiggle traces.
-
-This script has no user-interface capabilities. Its value is found in embedding it into tkinter UIs.
-
-TODO:
-    -- Put this in a namespace!
-
-"""
-
 import numpy
 import matplotlib
 import matplotlib.pyplot
@@ -30,7 +13,7 @@ trace = numpy.zeros(5000)
 for ii in range(0, len(trace)):
     trace[ii] = ricker_wavelet(len(trace)/10.0, ii - len(trace)/2.0)
 
-#matplotlib.pyplot.style.use('dark_background')
+matplotlib.pyplot.style.use('dark_background')
 fig, ax = matplotlib.pyplot.subplots()
 
 tAxis = numpy.linspace(0, len(trace)*0.001, len(trace))
