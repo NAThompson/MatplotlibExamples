a7e26509a3b698b0418faf35c67d4e5f9547b7a5
Nicholas <nthompson@slb.com>
2015-04-21 16:08:22 -0500

    Change style

--- wiggle_trace.py
+++ wiggle_trace.py
@@ -13,14 +13,24 @@ trace = numpy.zeros(5000)
 for ii in range(0, len(trace)):
     trace[ii] = ricker_wavelet(len(trace)/10.0, ii - len(trace)/2.0)
 
-matplotlib.pyplot.style.use('dark_background')
+#matplotlib.pyplot.style.use('dark_background')
+matplotlib.pyplot.style.use('fivethirtyeight')
+
 fig, ax = matplotlib.pyplot.subplots()
 
 tAxis = numpy.linspace(0, len(trace)*0.001, len(trace))
+
+# Add positive fill:
 ax.fill_between(trace, tAxis, where=trace>0)
+
+# Plot vertically by switching independent and dependent variables:
 ax.plot(trace, tAxis)
+
+# Make time increase downwards:
 matplotlib.pyplot.gca().invert_yaxis()
-matplotlib.pyplot.style.use('grayscale')
+
+#matplotlib.pyplot.style.use('grayscale')
+
 matplotlib.pyplot.show()
 
 
