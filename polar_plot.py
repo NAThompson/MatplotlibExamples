930e489a56f5e45ce8a546b4f6edded76ad1074a
Nicholas <nthompson@slb.com>
2015-04-21 16:01:09 -0500

    Improve polar plot

--- polar_plot.py
+++ polar_plot.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
 
-theta = numpy.linspace(0, 2*numpy.pi, 500)
-r = 2*numpy.sin(3*theta)
+theta = np.linspace(0, 2*np.pi, 500)
+r = 2*np.sin(3*theta)
 
 ax = plt.subplot(111, polar=True)
-ax.plot(theta, r, 'b-', theta, theta/(numpy.pi), 'g-')
+
+ax.plot(theta, r, 'b-', theta, theta/(np.pi), 'g-')
 ax.grid(True)
 
-ax.set_title(r"Rose Plot; $r = 2\sin(2\theta)$")
+ax.set_title(r"Rose Plot $r = 2\sin(2\theta)$ and Archimedean Spiral $r =\theta/2\pi$")
 plt.show()
