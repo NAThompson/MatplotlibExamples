4b104d9cdbb24afc12dfef06f482ad6ffeef0f6c
Nicholas <nthompson@slb.com>
2015-04-21 16:44:01 -0500

    Comments

--- mpl_connect.py
+++ mpl_connect.py
@@ -17,6 +17,8 @@ def process_button(event):
     print("Button:", event.x, event.y, event.xdata, event.ydata, event.button)
 
 fig, ax = plt.subplots(1, 1)
+
+# Define the callbacks:
 fig.canvas.mpl_connect('key_press_event', process_key)
 fig.canvas.mpl_connect('button_press_event', process_button)
 
